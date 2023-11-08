c73f76d60c8c5069264be14b3a0db64f6b50e110
KWensmann <kurtwensmann@gmail.com>
2023-11-08 09:08:30 -0600

    exported questions to questions.js

--- helpers/inquiries.js
+++ helpers/inquiries.js
@@ -1,91 +1,6 @@
 const inquirer = require("inquirer");
 const connection = require("../db/connection")
-
-
-
-
-let addEmployeesQuestions = [
-  {
-    type: "input",
-    message: "What is the employee's first name",
-    name: "first_name"
-  },
-  {
-    type: "input",
-    message: "What is the employee's last name?",
-    name: "last_name"
-  },
-  {
-    type: "list",
-    message: "What is the employee's role?",
-    name: "role_id",
-    choices: async () => {
-      const data = await connection.promise().query("SELECT * FROM role")
-      return data[0].map((role) => ({value: role.id, name: role.title}))
-    }
-  },
-  {
-    type: "list",
-    message: "Who is the employee's manager?",
-    name: "manager_id",
-    choices: async () => {
-      const data = await connection.promise().query("SELECT * FROM employee WHERE manager_id IS NULL");
-      return data[0].map((employee) => ({value: employee.id, name: employee.first_name + " " + employee.last_name}))
-    }
-  }
-]
-
-let updateEmployeeQuestions = [
-  {
-    type: "list",
-    message: "Which employee's role do you want to update?",
-    name: "employee_id",
-    choices: async () => {
-      const data = await connection.promise().query("SELECT * FROM employee");
-      return data[0].map((employee) => ({value: employee.id, name: employee.first_name + " " + employee.last_name}) )  
-    }
-  },
-  {
-    type: "list",
-    message: "Which role do you want to assign the selected employee?",
-    name: "role_id", 
-    choices: async() => {
-      const data = await connection.promise().query("SELECT * FROM role");
-      return data[0].map((role) => ({value: role.id, name: role.title}))
-    }
-  }
-]
-
-let addRoleQuestions = [
-  {
-    type: "input",
-    message: "What is the name of the role?",
-    name: "title"
-  },
-  {
-    type: "input",
-    message: "What is the salary of the role?", 
-    name: "salary",
-    validate: (input) => {
-      if(isNaN(input) || input.includes(",")){
-        return "Please enter a valid number without commas.";
-      }
-      if (Number(input) < 0) {
-        return "Please enter a positive number for the salary.";
-      }
-      return true;
-    }
-  },
-  {
-    type: "list",
-    message: "What is the role's Department?",
-    name: "department_id",
-    choices: async () => {
-      const data = await connection.promise().query("SELECT * FROM department");
-      return data[0].map((department) => ({value: department.id, name: department.name}))
-    }
-  }
-]
+const {updateEmployeeQuestions, addRoleQuestions, addEmployeesQuestions} = require("./questions")
 
 function start() {
   inquirer.prompt(
@@ -152,7 +67,7 @@ function addEmployee(){
     connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answers.first_name, answers.last_name, answers.role_id, answers.manager_id], (err, data) => {
       if (err) throw err;
       console.log("Employee added.");
-      displayRoles()
+      displayEmployees()
     })
   })
 }
---
+++ helpers/questions.js
@@ -0,0 +1,95 @@
+const connection = require("../db/connection")
+
+
+
+
+let addEmployeesQuestions = [
+  {
+    type: "input",
+    message: "What is the employee's first name",
+    name: "first_name"
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "last_name"
+  },
+  {
+    type: "list",
+    message: "What is the employee's role?",
+    name: "role_id",
+    choices: async () => {
+      const data = await connection.promise().query("SELECT * FROM role")
+      return data[0].map((role) => ({value: role.id, name: role.title}))
+    }
+  },
+  {
+    type: "list",
+    message: "Who is the employee's manager?",
+    name: "manager_id",
+    choices: async () => {
+      const data = await connection.promise().query("SELECT * FROM employee WHERE manager_id IS NULL");
+      let newData = data[0].map((employee) => ({value: employee.id, name: employee.first_name + " " + employee.last_name}))
+      newData.push({value: null, name: "None"});
+      return newData
+    }
+  }
+]
+
+let updateEmployeeQuestions = [
+  {
+    type: "list",
+    message: "Which employee's role do you want to update?",
+    name: "employee_id",
+    choices: async () => {
+      const data = await connection.promise().query("SELECT * FROM employee");
+      return data[0].map((employee) => ({value: employee.id, name: employee.first_name + " " + employee.last_name}) )  
+    }
+  },
+  {
+    type: "list",
+    message: "Which role do you want to assign the selected employee?",
+    name: "role_id", 
+    choices: async() => {
+      const data = await connection.promise().query("SELECT * FROM role");
+      return data[0].map((role) => ({value: role.id, name: role.title}))
+    }
+  }
+]
+
+let addRoleQuestions = [
+  {
+    type: "input",
+    message: "What is the name of the role?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "What is the salary of the role?", 
+    name: "salary",
+    validate: (input) => {
+      if(isNaN(input) || input.includes(",")){
+        return "Please enter a valid number without commas.";
+      }
+      if (Number(input) < 0) {
+        return "Please enter a positive number for the salary.";
+      }
+      return true;
+    }
+  },
+  {
+    type: "list",
+    message: "What is the role's Department?",
+    name: "department_id",
+    choices: async () => {
+      const data = await connection.promise().query("SELECT * FROM department");
+      return data[0].map((department) => ({value: department.id, name: department.name}))    
+    }
+  }
+]
+
+module.exports = {
+  addEmployeesQuestions,
+  addRoleQuestions,
+  updateEmployeeQuestions
+}
